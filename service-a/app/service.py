b99d9d87587da1aecc17e04d659ff08c3dbc09cf
yoseftaktuk <taktuk07@gmail.com>
2026-01-12 18:01:14 +0200

    add to getting_location_by_ip new_result varb in service.py

--- service-a/app/service.py
+++ service-a/app/service.py
@@ -4,9 +4,13 @@ from requests import HTTPError
 
 def getting_location_by_ip(ip: str):
     try:
+        new_result = {}
         response = requests.get(f'''http://ip-api.com/json/{ip}''')
         result = response.json()
-        return(result) 
+        new_result['lat'] = result['lat']
+        new_result['lon'] = result['lon']
+        new_result['ip'] = result['query']
+        return(new_result) 
     except HTTPError as e:
         raise str(e)
 
