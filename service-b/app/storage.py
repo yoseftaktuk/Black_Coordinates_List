7f26d7b88dc892d0470e09da6d5291a03762614b
Ariel_Izralevitch <your.email@example.com>
2026-01-13 12:35:11 +0200

    add env

--- service-b/app/storage.py
+++ service-b/app/storage.py
@@ -1,7 +1,11 @@
 import redis
+import os
+
+
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 
 redis_client = redis.Redis(
-    host="localhost",
+    host=REDIS_HOST,
     port=6379,
     decode_responses=True
 )
