897ff37166c7c5c3c80181482cdf3c9cf91cac63
yoseftaktuk <taktuk07@gmail.com>
2026-01-13 12:29:47 +0200

    add check_ip and check_coordinate func to schemas.py

--- service-a/app/routes.py
+++ service-a/app/routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, FastAPI, HTTPException
 import service
+import schemas
 
 app = FastAPI()
 router = APIRouter()
@@ -7,6 +8,7 @@ router = APIRouter()
 
 @router.post("/post_to_redit/{ip}")
 def read_users(ip: str):
+    schemas.check_ip(ip)
     try:
         result  = service.getting_location_by_ip(ip=ip)
         send = service.send_to_service_b(result)
--- service-a/app/service.py
+++ service-a/app/service.py
@@ -1,12 +1,13 @@
 import requests
 from requests import HTTPError
-
+import schemas
 BASEURL = 'http://192.168.20.179:8080'
 
 def getting_location_by_ip(ip: str):
     try:
         response = requests.get(f'''http://ip-api.com/json/{ip}''')
         result = response.json()
+        schemas.check_coordinate(result['lat'], result['lon'])
         return {'ip': result['query'], 'longitude':result['lon'], 'latitude':  result['lat']}
     except HTTPError as e:
         return {'message': str(e)}
