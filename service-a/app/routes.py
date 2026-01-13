9e588ab8efe20eef6d6fafec3dc4a508109c8fc4
yoseftaktuk <taktuk07@gmail.com>
2026-01-13 11:23:17 +0200

    add to root read_users in routes.py try and except

--- service-a/app/routes.py
+++ service-a/app/routes.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter, FastAPI, HTTPException
 import service
-import requests
-from requests import HTTPError 
+
 app = FastAPI()
 router = APIRouter()
 
 
 @router.post("/post_to_redit/{ip}")
 def read_users(ip: str):
-    result  = service.getting_location_by_ip(ip=ip)
-    send = service.send_to_service_b(result)
-    return send
+    try:
+        result  = service.getting_location_by_ip(ip=ip)
+        send = service.send_to_service_b(result)
+        return send
+    except HTTPException as e:
+        return {'message': str(e)}
+
 
